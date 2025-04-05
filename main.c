5410a8ab10d8ecf1f1f872e8bcb718f9d956d0b9
Guilherme <72567457+guibr29p@users.noreply.github.com>
2025-04-04 21:15:47 -0300

    maker menu

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h> 
+#include <stdio.h>
+
+
+int main(){
+    int escolha;
+    
+    printf("1 - Licoes");
+    printf("2 - Exercicios");
+    printf("3 - Avaliacao");
+    printf("4 - Ranker")
+    
+    swtiche(){
+    }
+
+    return 0;
+}
