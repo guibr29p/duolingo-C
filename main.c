bf2a80d983b0ce9ac2d10488907d5afdf30ea7ad
Guilherme <72567457+guibr29p@users.noreply.github.com>
2025-04-09 21:53:52 +0000

    feature

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h> 
 #include <stdio.h>
 
+int strcmp(char *str1);
+int login();
 
 int main(){
-    int escolha;
-    
-    printf("1 - Licoes");
+    int escolha, usuario_logado;
+    login();
+    /*printf("1 - Licoes");
     printf("2 - Exercicios");
     printf("3 - Avaliacao");
     printf("4 - Ranker");
@@ -18,9 +20,31 @@ int main(){
         case 2:
             printf("exerciciocs");
         break;
-        default
+        default:
             printf("comando invalido");
-    }
+    }*/
+    return 0;
+}
 
+
+int login(/*char *user,int senha*/){
+    char user[3][50] = {
+        "joao",
+        "guilherme",
+        "wilian",
+    };
+
+    int senha[3] = {
+        1234567
+    };
+
+    for(int i = 0; i < 3; i++){
+        strcmp(user[i]);
+    }
+    return 0;
+}
+
+int strcmpc(char *str1 /*, char *str2*/){
+    
     return 0;
 }
