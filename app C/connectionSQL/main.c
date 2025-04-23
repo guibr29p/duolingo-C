2f7e8b81b1488f4349b8e37883bb3a3480121c78
Guilherme <72567457+guibr29p@users.noreply.github.com>
2025-04-23 13:24:26 -0300

    seach_aluno function

--- app C/connectionSQL/main.c
+++ app C/connectionSQL/main.c
@@ -5,7 +5,7 @@
 #include <windows.h>
 #include "mysql\include\mysql.h"
 
-void inserir(MYSQL *conexao);
+
 void erro(MYSQL *conexao){
     fprintf(stderr,  "\n%s\n", mysql_error(conexao));
     mysql_close(conexao);
@@ -23,56 +23,109 @@ MYSQL *__stdcall obterconexao(){
 
     if(!mysql_real_connect(conexao, servidor, usuario, senha, nomebanco, porta, NULL, 0)){
         erro(conexao);
+        return NULL;
     }
     else{
         return conexao;
     }
 }
 
-int login(MYSQL *conexao, char *registrer, char *senha){
+int login(MYSQL *conexao){
     MYSQL_RES *resultado;
     MYSQL_ROW row;
-    int encontrado = 0;
-    char *query;
-    sprintf(query, "SELECT * FROM usuarios WHERE (nome = '%s' or email = '%s') and senha = '%s'", registrer, registrer, senha);
+    int encontrado = 0, RA;
+    char query[750], senha[100];
+    printf("digite seu RA: ");
+    scanf("%i", &RA);
+    printf("digite sua senha: ");
+    scanf("%s", &senha);
+    sprintf(query, "SELECT * FROM aluno WHERE ra = %i and senha = '%s'", RA, senha);
     if(mysql_query(conexao, query)){
         erro(conexao);
     }
     resultado = mysql_store_result(conexao);
-    while ((row = mysql_fetch_row(resultado)) != NULL)
+    while((row = mysql_fetch_row(resultado)) != NULL)
     {
        printf("\nbem vindo %s \n", row[1]);
+       verificacao_acesso(row[2], row[4]);
        return encontrado = 1;
     } 
-    if (!encontrado) {
-        printf("Usuário ou senha inválidos.\n");
+    if (!encontrado){
+        printf("RA ou senha invalido.\n");
+        return encontrado = 0;
     } 
     mysql_free_result(resultado);
 }
 
-int main(){
-    MYSQL *conexao = obterconexao();
-    char nome[100], senha[100];
-    printf("digite seu nome ou emai: ");
+void inserir(MYSQL *conexao){ // adicionar alunos na tabela
+    char nome[250], senha[100], resp, query[750];
+    printf("insira o nome do aluno: ");
     scanf("%s", &nome);
-    printf("digite sua senha: ");
-    scanf("%s", &senha);
-    
-    if(login(conexao, nome, senha) == 1){
-        printf("menu ");
-    }
-    return 0;
-}
-
-
-void inserir(MYSQL *conexao){
-
-    char query[750];
-    sprintf(query, "INSERT INTO usuario(nome, senha) VELUES (%s, %s)");
-    if(mysql_query(query)){
+    sprintf(query, "INSERT INTO aluno(nome) VALUES ('%s')", nome);
+    if(mysql_query(conexao, query)){
         erro(conexao);
     }
     else{
-        printf("aluno cadastrado")
+        printf("aluno cadastrado");
     }
+}
+
+void remover(MYSQL *conexao){
+    MYSQL_RES *res;
+    MYSQL_ROW *row;
+    int ra;
+    char query[250], resp;
+    printf("digite o RA do aluno: ");
+    scanf("%i", &ra);
+    sprintf(query, "SELECT ra, nome FROM aluno WHERE ra = %i", ra);
+    if(mysql_query(conexao, query)){
+        erro(conexao);
+    }
+    res = mysql_store_result(conexao);
+    while((row = mysql_fetch_row(res)) != NULL){
+        printf("RA: %i \n", row[0]);
+        printf("nome: %s \n", row[1]);
+    }
+    mysql_free_result(res);
+    sprintf(query, "DELETE FROM aluno WHERE ra = %i", ra);
+    printf("gostaria de apagar o aluno (s/n)\n");
+    scanf("%c", &resp);
+    if(resp == "s"){
+        if(mysql_query(conexao, query)){
+            erro(conexao);    
+        }
+        else{
+            printf("aluno romovido");
+        }
+    }
+    
+}
+void verificacao_acesso(char *email, char *senha){
+    if(email == NULL || senha == ""){
+        printf("1");
+    }
+}
+
+int main(){
+    MYSQL *conexao = obterconexao();
+    int navegacao;
+    
+    if(login(conexao) == 1){
+        printf("1-insirir aluno \n");
+        printf("2-remover aluno \n");
+        scanf("%i", &navegacao);
+        switch (navegacao)
+        {
+        case 1:
+            inserir(conexao);
+            break;
+        case 2:
+            remover(conexao);
+            break;
+        default:
+            break;
+        }
+    } 
+    system("pause");
+    return 0;
 }
